f58665ba08c71dfbe83ee7aa8ea35488704438a4
Chandler L <cbl0015@auburn.edu>
2020-07-22 23:19:50 -0400

    Initial JS framework

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// TBD
+var previousLoc = [];
+var currentLoc;
+
+function initialize() {
+    //uses old locations from local storage
+    previousLoc = JSON.parse(localStorage.getItem("weathercities"));
+    var lastSearch;
+    //shows buttons from old searches
+    if (previousLoc) {
+        //grabs the most recently searched city 
+        currentLoc = previousLoc[previousLoc.length - 1];
+        showPrevious();
+        getCurrent(currentLoc);
+    }
+    else {
+        //try to locate, otherwise set default as Atlanta
+        if (!navigator.geolocation) {
+            //no input = Atlanta
+            getCurrent("Atlanta");
+        }
+        else {
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+    }
+
+}
