477a9daecc7ade893e8cad84b61b0cd30d682f8d
Chandler L <cbl0015@auburn.edu>
2020-07-23 02:39:10 -0400

    Further JS funtionality additions

--- script.js
+++ script.js
@@ -24,3 +24,43 @@ function initialize() {
     }
 
 }
+
+function success(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=f4d4de4a53ae884dfc78b96e481c919f";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        currentLoc = response.name;
+        saveLoc(response.name);
+        getCurrent(currentLoc);
+    });
+
+}
+
+function error(){
+    //If locating fails, automatically give them Atlanta
+    currentLoc = "Atlanta"
+    getCurrent(currentLoc);
+}
+
+function showPrevious() {
+    //Use local storage to display old searches
+    if (previousLoc) {
+        $("#prevSearches").empty();
+        var btns = $("<div>").attr("class", "list-group");
+        for (var i = 0; i < previousLoc.length; i++) {
+            var locBtn = $("<a>").attr("href", "#").attr("id", "loc-btn").text(previousLoc[i]);
+            if (previousLoc[i] == currentLoc){
+                locBtn.attr("class", "list-group-item list-group-item-action active");
+            }
+            else {
+                locBtn.attr("class", "list-group-item list-group-item-action");
+            }
+            btns.prepend(locBtn);
+        }
+        $("#prevSearches").append(btns);
+    }
+}
